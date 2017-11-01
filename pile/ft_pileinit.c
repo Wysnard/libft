5317d728bd7214dda4a61fdf18baeaade5a00150
Wysnard <Wysnard@users.noreply.github.com>
2017-11-01 01:55:01 +0100

    maj

--- pile/ft_pileinit.c
+++ pile/ft_pileinit.c
@@ -1,10 +1,11 @@
 #include "../libft.h"
 
-void	ft_pileinit(void)
+Pile	*ft_pileinit(void)
 {
 	Pile *pile;
 
 	pile = malloc(sizeof(*pile));
 	pile->first = NULL;
 	pile->info = NULL;
+	return (pile);
 }
