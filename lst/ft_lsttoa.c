f098fd59227cc926c5c8e8740d20e172cef6317a
Wysnard <Wysnard@users.noreply.github.com>
2017-11-03 18:28:51 +0100

    maj

--- lst/ft_lsttoa.c
+++ lst/ft_lsttoa.c
@@ -7,15 +7,20 @@ char	*ft_lsttoa(t_list *list)
 	char	*result;
 
 	len = 0;
-	tmp = list;
+	if (!(tmp = list))
+		return (NULL);
 	while (tmp)
 	{
+		if (list->content == NULL)
+			break ;
 		len += ft_strlen(list->content);
 		tmp = tmp->next;
 	}
 	result = ft_strnew(len);
 	while (list)
 	{
+		if (list->content == NULL)
+			break ;
 		ft_strcat(result, list->content);
 		list = list->next;
 	}
