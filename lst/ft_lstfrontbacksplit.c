0652e8c0f9126b2cdec300b5942350104bc23735
Wysnard <Wysnard@users.noreply.github.com>
2017-11-04 19:00:37 +0100

    lstfrontbacksplit

---
+++ lst/ft_lstfrontbacksplit.c
@@ -0,0 +1,28 @@
+#include "../libft.h"
+
+void	ft_lstfrontbacksplit(t_list *src, t_list **a, t_list **b)
+{
+	t_list	*slow;
+	t_list	*fast;
+
+	if (src == NULL || src->next == NULL)
+	{
+		a = src;
+		b = NULL;
+		return ;
+	}
+	slow = src;
+	fast = src;
+	while (fast)
+	{
+		fast = fast->next;
+		if (fast)
+		{
+			slow = slow->next;
+			fast = fast->next;
+		}
+		a = src;
+		b = slow->next;
+		slow->next = NULL;
+	}
+}
