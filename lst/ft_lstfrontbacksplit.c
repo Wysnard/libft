325e5bc9517c823597aadae2147c754a5f16eddf
Wysnard <Wysnard@users.noreply.github.com>
2017-11-05 19:03:00 +0100

    maj

--- lst/ft_lstfrontbacksplit.c
+++ lst/ft_lstfrontbacksplit.c
@@ -4,9 +4,9 @@ void ft_lstfrontbacksplit(t_list* source, t_list** frontRef, t_list** backRef)
 {
 	t_list* fast;
 	t_list* slow;
+	
 	if (source==NULL || source->next==NULL)
 	{
-		/* length < 2 cases */
 		*frontRef = source;
 		*backRef = NULL;
 	}
@@ -15,7 +15,6 @@ void ft_lstfrontbacksplit(t_list* source, t_list** frontRef, t_list** backRef)
 		slow = source;
 		fast = source->next;
 
-		/* Advance 'fast' two nodes, and advance 'slow' one node */
 		while (fast != NULL)
 		{
 			fast = fast->next;
@@ -25,9 +24,6 @@ void ft_lstfrontbacksplit(t_list* source, t_list** frontRef, t_list** backRef)
 				fast = fast->next;
 			}
 		}
-
-		/* 'slow' is before the midpoint in the list, so split it in two
-			at that point. */
 		*frontRef = source;
 		*backRef = slow->next;
 		slow->next = NULL;
