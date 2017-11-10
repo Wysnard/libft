ab8a72e92bc538a382d7cfe79ce324b7dbd3c3c8
Wysnard <Wysnard@users.noreply.github.com>
2017-11-10 03:55:42 +0100

    fix

--- btree/ft_btree_rev_infix.c
+++ btree/ft_btree_rev_infix.c
@@ -5,8 +5,8 @@ void	ft_btree_rev_infix(t_btree *root, void(*applyf)(void *))
 	if (root == NULL)
 		return ;
 	if (root->right != NULL)
-		ft_btree_apply_infix(root->right, applyf);
+		ft_btree_rev_infix(root->right, applyf);
 	(*applyf)(root->content);
 	if (root->left != NULL)
-		ft_btree_apply_infix(root->left, applyf);
+		ft_btree_rev_infix(root->left, applyf);
 }
