e8fedca7824d3f88b7ced421bccaf52fbd56ff6f
Wysnard <Wysnard@users.noreply.github.com>
2017-11-10 02:45:34 +0100

    fix

--- btree/ft_btreesearch.c
+++ btree/ft_btreesearch.c
@@ -6,7 +6,7 @@ void *ft_btreesearch(t_btree *root, void *data_ref, int (*cmpf)(void *, void *))
     return (NULL);
   if (root->left != NULL)
     ft_btreesearch(root->left, data_ref, cmpf);
-  if ((*cmpf)(root->item, data_ref) == 0)
+  if ((*cmpf)(root->content, data_ref) == 0)
     return (root->content);
   if (root->right != NULL)
     ft_btreesearch(root->right, data_ref, cmpf);
