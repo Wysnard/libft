497e5b67f22dc497f4ef80ce23f68243aca4dd6b
Wysnard <Wysnard@users.noreply.github.com>
2017-11-10 04:02:32 +0100

    del

--- btree/ft_btreedel.c
+++ btree/ft_btreedel.c
@@ -7,5 +7,7 @@ void	ft_btreedel(t_btree *tr)
 	//ft_printf("suppression de %s\n", tr->item);
 	ft_btreedel(tr->left);
 	ft_btreedel(tr->right);
+	if (tr->content)
+		free(tr->content);
 	free(tr);
 }
